cf72c6bcfc322c9af740b972c347f09053f8343d
HazaelLopez <hazael@HazaelLopez.HazaelLopez.edu>
2021-02-23 11:57:20 -0600

    Validaciones del usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,14 +4,26 @@ const Usuario = require('../models/Usuario');
 
 const crearUsuario = async (req, res = response ) =>{
 
-    // const { name, email, password } = req.body;
+    const { email, password } = req.body;
+
+
+
     try {
-        const usuario = new Usuario( req.body);
+        let usuario = await Usuario.findOne({ email });
+        if( usuario ) {
+            return res.status(400).json({
+                ok: false,
+                msg: "El usario ya existe con este correo"
+            });
+        }
+
+        usuario = new Usuario( req.body);
         await usuario.save();
 
         res.status(201).json({
             ok: true,
-            msg: "Register"
+            uid: usuario.id,
+            name: usuario.name
         })
     } catch (error) {
         console.log(error)
