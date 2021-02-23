76b1d2da533f1c73bf4149acfcb0722112add1be
HazaelLopez <hazael@HazaelLopez.HazaelLopez.edu>
2021-02-23 09:24:03 -0600

    Variables de entorno y carpeta pública

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require('express');
+require('dotenv').config();
 
 // Crear el servidor de express
 const app = express();
 
+
+// Directorio publico
+app.use(express.static('public'));
+
 // Rutas
+/*
 app.get('/', (req, res) =>{
     res.json({
         ok: true,
         msg: "Petición correcta"
     })
-})
+})*/
 
 
 // Escuchar peticiones
-app.listen( 4000, () => {
-    console.log(`Servidor corriendo en puerto ${4000}`);
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
 });
