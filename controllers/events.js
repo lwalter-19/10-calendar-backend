439c91706dcd7b11f6d8a0598cd6a116212239d5
HazaelLopez <hazael@HazaelLopez.HazaelLopez.edu>
2021-02-24 09:51:18 -0600

    Actualizar un Evento

--- controllers/events.js
+++ controllers/events.js
@@ -34,11 +34,52 @@ const crearEvento = async (req, res = response ) => {
     }
 }
 
-const actualizarEvento = (req, res = response ) => {
+const actualizarEvento = async (req, res = response ) => {
+
+    const eventoId = req.params.id;
+
+    try {
+
+        const evento = await Evento.findById(eventoId);
+        const uid = req.uid;
+
+        if( !evento ){
+            res.status(404).json({
+                ok: false,
+                msg: "Evento no existe con este id"
+            })
+        }
+
+        if( evento.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: "No tiene privilegio de editar este evento"
+            });
+        }
+
+        const nuevoEvento = {
+            ...req.body,
+            user: uid
+        }
+
+        const eventoActualizado = await Evento.findByIdAndUpdate( eventoId, nuevoEvento, { new: true} );
+
+        res.json({
+            ok: true,
+            evento: eventoActualizado
+        });
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: "Hable con el administrador"
+        });
+    }
 
     res.status(200).json({
         ok: true,
-        msg: "actualizarEvento"
+        eventoId
     });
 }
 
