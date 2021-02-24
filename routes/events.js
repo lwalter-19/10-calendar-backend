a2114e1f236aa84cf458d15a43e13dab8ba935d7
HazaelLopez <hazael@HazaelLopez.HazaelLopez.edu>
2021-02-24 00:26:12 -0600

    Validar campos necesarios

--- routes/events.js
+++ routes/events.js
@@ -6,6 +6,9 @@ const { Router } = require('express');
 const router = Router();
 const { validarJWT } = require('../middlewares/validar-jwt');
 const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { isDate } = require('../helpers/isDate');
 
 
 // Todas tiene que pasar por JWT
@@ -15,7 +18,16 @@ router.use( validarJWT );
 router.get('/', getEventos);
 
 
-router.post('/', crearEvento);
+router.post(
+    '/', 
+    [ // validar nuestro campos
+        check('title', 'El titulo es obligatori').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom( isDate ),
+        check('end', 'Fecha de finalizacion es obligatoria').custom( isDate ),
+        validarCampos
+    ],
+    crearEvento
+);
 
 
 router.put('/:id',  actualizarEvento);
