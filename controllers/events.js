3cfb69f3e20998d898e5c080be6554172401c1e9
HazaelLopez <hazael@HazaelLopez.HazaelLopez.edu>
2021-02-24 10:02:02 -0600

    Eliminar Eventos

--- controllers/events.js
+++ controllers/events.js
@@ -37,14 +37,14 @@ const crearEvento = async (req, res = response ) => {
 const actualizarEvento = async (req, res = response ) => {
 
     const eventoId = req.params.id;
+    const uid = req.uid;
 
     try {
 
         const evento = await Evento.findById(eventoId);
-        const uid = req.uid;
 
         if( !evento ){
-            res.status(404).json({
+            return res.status(404).json({
                 ok: false,
                 msg: "Evento no existe con este id"
             })
@@ -83,12 +83,40 @@ const actualizarEvento = async (req, res = response ) => {
     });
 }
 
-const eliminarEvento = (req, res = response ) => {
+const eliminarEvento = async (req, res = response ) => {
 
-    res.status(200).json({
-        ok: true,
-        msg: "eliminarEvento"
-    });
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+    try {
+
+        const evento = await Evento.findById(eventoId);
+
+        if( !evento ){
+            return res.status(404).json({
+                ok: false,
+                msg: "Evento no existe con este id"
+            })
+        }
+
+        if( evento.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: "No tiene privilegio de eliminar este evento"
+            });
+        }
+
+        await Evento.findByIdAndDelete( eventoId );
+
+        res.json({ ok: true  });
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: "Hable con el administrador"
+        });
+    }
 }
 
 
