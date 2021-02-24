02f4bddc632ae9009746d4908b06c11192eaabef
HazaelLopez <hazael@HazaelLopez.HazaelLopez.edu>
2021-02-23 23:59:45 -0600

    Resolución de la tarea - CRUD

--- routes/events.js
+++ routes/events.js
@@ -1,18 +1,24 @@
+/*  
+    Event Routers
+    path -> /api/events
+*/
 const { Router } = require('express');
 const router = Router();
 const { validarJWT } = require('../middlewares/validar-jwt');
+const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
 
-validarJWT();
 
 // Todas tiene que pasar por JWT
 // Crear una peticion para obtener eventos
-router.get('/', getEventos);
+router.get('/', validarJWT, getEventos);
 
 
-router.post('/', crearEvento);
+router.post('/', validarJWT, crearEvento);
 
 
-router.put('/:id', actualizarEvento);
+router.put('/:id', validarJWT,  actualizarEvento);
 
 
-router.delete('/:id', eliminarEvento);
+router.delete('/:id', validarJWT, eliminarEvento);
+
+module.exports = router
